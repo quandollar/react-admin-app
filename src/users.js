aa6586892e5ef50452b0754d9cb45b2362aa58d2
Quan Nguyen <quanwnn@gmail.com>
2022-07-15 22:37:01 -0400

    show users data table

---
+++ src/users.js
@@ -0,0 +1,15 @@
+import * as React from "react";
+import { List, Datagrid, TextField, EmailField } from 'react-admin';
+
+export const UserList = () => (
+    <List>
+        <Datagrid rowClick="edit">
+            <TextField source="id" />
+            <TextField source="name" />
+            <EmailField source="email" />
+            <TextField source="phone" />
+            <TextField source="website" />
+            <TextField source="company.name" />
+        </Datagrid>
+    </List>
+);
